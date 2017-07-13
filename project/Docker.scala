75591bace08d949f0eee54f63b5fc70fbece180e
matthedude <matthedude@hotmail.com>
2017-07-13 14:24:19 +0100

    Only allow push to registry if not a SNAPSHOT version. Also only tag as latest if not SNAPSHOT.

--- project/Docker.scala
+++ project/Docker.scala
@@ -1,18 +1,29 @@
 import com.typesafe.sbt.packager.Keys._
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.{Docker => docker}
+import sbt.Def
+import sbt.Keys._
 
 object Docker {
 
   lazy val dockerSettings = Seq(
     packageName in docker := packageName.value,
     dockerBaseImage := "anapsix/alpine-java:8",
-    dockerUpdateLatest := true,
-    dockerRepository := registryWithRepository("sky")
+    dockerUpdateLatest := updateLatest.value,
+    dockerRepository := registryWithRepository("sky").value
   )
 
-  def registryWithRepository(repository: String) = {
-    val registry = sys.env.get("DOCKER_REGISTRY_HOST").map(_ + "/").getOrElse("")
-    Some(s"$registry$repository")
+  def registryWithRepository(repository: String) = Def.setting {
+    if (updateLatest.value) {
+      val registry = sys.env.get("DOCKER_REGISTRY_HOST").map(_ + "/").getOrElse("")
+      Some(s"$registry$repository")
+    } else {
+      Some(repository)
+    }
+  }
+
+  def updateLatest = Def.setting {
+    if (!version.value.contains("SNAPSHOT")) true
+    else false
   }
 }
 
