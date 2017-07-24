69775da90065ee15dd5ab45a3bc8779021d072a0
matthedude <matthedude@hotmail.com>
2017-07-24 13:36:22 +0100

    Run ciRelease if on master. Reset values from prev commit that were for testing the Travis docker push.

--- project/Aliases.scala
+++ project/Aliases.scala
@@ -3,7 +3,7 @@ import sbt._
 object Aliases {
 
   lazy val defineCommandAliases = {
-    addCommandAlias("ciBuild", ";clean; test; docker:publish") ++
+    addCommandAlias("ciBuild", ";clean; test") ++
       addCommandAlias("ciRelease", ";clean; release with-defaults")
   }
 }
