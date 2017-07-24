c8e199a0b13912678eae7bd571206ad6aff57ebd
matthedude <matthedude@hotmail.com>
2017-07-24 13:15:01 +0100

    Trying out docker integration with Travis.

--- project/Aliases.scala
+++ project/Aliases.scala
@@ -3,7 +3,7 @@ import sbt._
 object Aliases {
 
   lazy val defineCommandAliases = {
-    addCommandAlias("ciBuild", ";clean; test") ++
+    addCommandAlias("ciBuild", ";clean; test; docker:publish") ++
       addCommandAlias("ciRelease", ";clean; release with-defaults")
   }
 }
