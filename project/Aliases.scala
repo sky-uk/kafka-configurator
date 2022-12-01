ab728421b6b4779612e3b21640f3e50cf0f0ff45
Ben Carter <ben.carter@sky.uk>
2022-12-01 16:39:45 +0000

    Update aliases

--- project/Aliases.scala
+++ project/Aliases.scala
@@ -2,8 +2,10 @@ import sbt._
 
 object Aliases {
 
-  lazy val defineCommandAliases = {
-    addCommandAlias("ciBuild", ";clean; test") ++
-      addCommandAlias("ciRelease", ";clean; release with-defaults")
-  }
+  lazy val defineCommandAliases: Seq[Def.Setting[State => State]] =
+    addCommandAlias("checkFix", "scalafixAll --check OrganizeImports; scalafixAll --check") ++
+      addCommandAlias("runFix", "scalafixAll OrganizeImports; scalafixAll") ++
+      addCommandAlias("checkFmt", "scalafmtCheckAll; scalafmtSbtCheck") ++
+      addCommandAlias("runFmt", "scalafmtAll; scalafmtSbt") ++
+      addCommandAlias("ciBuild", "clean; checkFmt; checkFix; test")
 }
